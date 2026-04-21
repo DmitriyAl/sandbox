f3cff71637df8dc8aafcf4e0399ccd2336a58113
Albot Dmitriy <Albot@gnivc.ru>
2026-04-21 10:44:19 +0300

    kafka

--- kafka/consumer/src/main/java/io/microservices/consumer/service/MessageConsumerService.java
+++ kafka/consumer/src/main/java/io/microservices/consumer/service/MessageConsumerService.java
@@ -1,5 +1,7 @@
 package io.microservices.consumer.service;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,6 +11,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class MessageConsumerService {
+    private final Counter succesCounter;
+    private final Counter failureCounter;
+
+    public MessageConsumerService(MeterRegistry meterRegistry) {
+        this.succesCounter = Counter.builder("kafka_consumer_success_counter")
+                .description("Success consumer counter")
+                .tags("consumer", "success")
+                .register(meterRegistry);
+        this.failureCounter = Counter.builder("kafka_consumer_failure_counter")
+                .description("Failure consumer counter")
+                .tags("consumer", "failure")
+                .register(meterRegistry);
+    }
 
     /**
      * Слушатель для топика test-topic
@@ -39,8 +54,10 @@ public class MessageConsumerService {
             // Ручное подтверждение успешной обработки
             acknowledgment.acknowledge();
             log.info("✅ Сообщение успешно обработано и подтверждено");
+            succesCounter.increment();
         } catch (Exception e) {
             log.error("❌ Ошибка при обработке сообщения: {}", e.getMessage(), e);
+            failureCounter.increment();
             // В зависимости от требований: можно не подтверждать (сообщение будет перечитано)
             // или сохранить в DLQ (Dead Letter Queue)
         }
--- kafka/producer/src/main/java/io/microservices/producer/service/KafkaProducerService.java
+++ kafka/producer/src/main/java/io/microservices/producer/service/KafkaProducerService.java
@@ -1,15 +1,29 @@
 package io.microservices.producer.service;
 
-import lombok.RequiredArgsConstructor;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class KafkaProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final Counter succesCounter;
+    private final Counter failureCounter;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate, MeterRegistry meterRegistry) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.succesCounter = Counter.builder("kafka_producer_success_counter")
+                .description("Success producer counter")
+                .tags("producer", "success")
+                .register(meterRegistry);
+        this.failureCounter = Counter.builder("kafka_producer_failure_counter")
+                .description("Failure producer counter")
+                .tags("producer", "failure")
+                .register(meterRegistry);
+    }
 
     public void sendMessage(String topic, String message) {
         log.info("Sending message to topic {}: {}", topic, message);
@@ -17,8 +31,10 @@ public class KafkaProducerService {
                 .whenComplete((result, ex) -> {
                     if (ex == null) {
                         log.info("Message sent successfully to topic {}", topic);
+                        succesCounter.increment();
                     } else {
                         log.error("Failed to send message to topic {}", topic, ex);
+                        failureCounter.increment();
                     }
                 });
     }
