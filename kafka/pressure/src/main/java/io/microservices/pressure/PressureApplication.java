85b65d712bc8caf75018412f677eb0d9adfa6eff
Albot Dmitriy <Albot@gnivc.ru>
2026-04-21 15:35:17 +0300

    pressure

---
+++ kafka/pressure/src/main/java/io/microservices/pressure/PressureApplication.java
@@ -0,0 +1,13 @@
+package io.microservices.pressure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PressureApplication.class, args);
+    }
+
+}
---
+++ kafka/pressure/src/test/java/io/microservices/pressure/PressureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.microservices.pressure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
