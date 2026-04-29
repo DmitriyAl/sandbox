9fdb2aa6a935f975fe4678f36408f483d025234a
Albot Dmitriy <Albot@gnivc.ru>
2026-04-29 14:34:41 +0300

    kafka

--- kafka/build_deploy.bat
+++ kafka/build_deploy.bat
@@ -5,22 +5,22 @@ echo %PRODUCER_VER%
 
 cd consumer
 @REM
-docker build -t kafka-consumer:%CONSUMER_VER% .
-minikube image load kafka-consumer:%CONSUMER_VER%
+@REM docker build -t kafka-consumer:%CONSUMER_VER% .
+@REM minikube image load kafka-consumer:%CONSUMER_VER%
 @REM
 cd chart
 
-helm install my-kafka-consumer .
+helm upgrade my-kafka-consumer .
 
 cd ../..
 @REM
 cd producer
 @REM
-docker build -t kafka-producer:%PRODUCER_VER% .
-minikube image load kafka-producer:%PRODUCER_VER%
+@REM docker build -t kafka-producer:%PRODUCER_VER% .
+@REM minikube image load kafka-producer:%PRODUCER_VER%
 @REM
 cd chart
 @REM
-helm install my-kafka-producer .
+helm upgrade my-kafka-producer .
 @REM
 cd ../..
