f826d1eec02fbc877ca9c32ab9595be21f5e4431
Albot Dmitriy <Albot@gnivc.ru>
2026-07-10 09:22:54 +0300

    elk

--- helm/elk-8.5.1/start.bat
+++ helm/elk-8.5.1/start.bat
@@ -1,4 +1,4 @@
 kubectl create namespace logging
-helm install elasticsearch ./elasticsearch --namespace logging --set replicas=1 --set minimumMasterNodes=1 --set resources.requests.cpu="100m" --set resources.requests.memory="512Mi" --set resources.limits.cpu="1000m" --set resources.limits.memory="2Gi" --set discovery.type=single-node
+helm install elasticsearch ./elasticsearch --namespace logging --set replicas=1 --set minimumMasterNodes=1 --set resources.requests.cpu="100m" --set resources.requests.memory="512Mi" --set resources.limits.cpu="1000m" --set resources.limits.memory="4Gi" --set discovery.type=single-node
 helm install kibana ./kibana --namespace logging --set elasticsearchHosts="https://elasticsearch-master:9200" --set resources.requests.memory="256Mi" --set resources.limits.memory="512Mi"
 helm install filebeat ./filebeat --namespace logging --set elasticsearchHosts="https://elasticsearch-master:9200" -f ./filebeat/values-json.yaml
